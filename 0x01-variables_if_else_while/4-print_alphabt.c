f62fa95d3bb6d3ef5ae9c75e69185472e9c882cd
SOLL <ibedemudo23@gmail.com>
2022-04-12 02:19:36 +0100

    Variables

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,4 +1,4 @@
-$ cat 4-print_alphabt.c
+File: 4-print_alphabt.c
 /*
  * File: 4-print_alphabt.c
  * Auth: Brennan D Baraban
@@ -13,13 +13,12 @@ $ cat 4-print_alphabt.c
  */
 int main(void)
 {
-        char letter;
-
-        for (letter = 'a'; letter <= 'z'; letter++)
-        {
-                if (letter != 'e' && letter != 'q')
-                        putchar(letter);
-        }
+char letter;
+for (letter = 'a'; letter <= 'z'; letter++)
+{
+if (letter != 'e' && letter != 'q')
+putchar(letter);
+}
 
         putchar('\n');
 
