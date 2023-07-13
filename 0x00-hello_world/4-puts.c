318738059a802b4a330d39b154262f98d6b0dcfe
SOLL <ibedemudo23@gmail.com>
2023-07-13 15:59:14 -0700

    Ox00-Hello-world

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A projrwm that prints with puts function
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is the building a multilingual puzzle");
+return (0);
+}
