126494b03b2853d084862fe941c7c44821190ed3
SOLL <ibedemudo23@gmail.com>
2023-07-13 23:04:45 -0700

    Low level programming

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,12 +5,13 @@
  *  return 0 (success)
  */
 int main(void)
-(
+{
  	char a;
 	int b;
 	long int c;
 	long long int d;
 	float f;
+
 	printf("Size of char: %1u byte(s)\n", (unsigned long)sizeof(a));
 	printf("Size of an int: %1u byte(s)\n", (unsigned long)sizeof(b));
 	printf("Size of a long int: %1u byte(s)\n", (unsigned long)sizeof(c));
