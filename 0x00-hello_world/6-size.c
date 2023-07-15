82ef9b02c61475e8f21198c300ab6ffbfe45736c
SOLL <ibedemudo23@gmail.com>
2023-07-15 12:00:30 -0700

    Low level programming

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
 /**
- *
  *  main - A program that prints the size of various types of computer
  *  return 0 (success)
  */
 int main(void)
 {
- 	char a;
+	char a;
 	int b;
 	long int c;
 	long long int d;
 	float f;
 
-	printf("Size of char: %ld byte(s)\n", (unsigned long)sizeof(a));
+	printf("Size of a char: %ld byte(s)\n", (unsigned long)sizeof(a));
 	printf("Size of an int: %ld byte(s)\n", (unsigned long)sizeof(b));
 	printf("Size of a long int: %ld byte(s)\n", (unsigned long)sizeof(c));
 	printf("Size of long long int: %ld byte(s)\n", (unsigned long)sizeof(d));
