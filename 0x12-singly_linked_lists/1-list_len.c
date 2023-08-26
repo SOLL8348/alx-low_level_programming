c99ee9dd73d4c8a0b9e561ac22691f5dfadc34d3
SOLL <ibedemudo23@gmail.com>
2023-08-25 21:44:12 -0700

    0x12. C

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,21 +1,21 @@
-nclude "lists.h"
+#include "lists.h"
 
 /** 
  * list_len - finds the number of elements in a linked list 
  * @h: pointer to the list 
  * 
  * Return: the number of elements 
- **/ 
-  
- size_t list_len(const list_t *h) 
+ **/
+
+size_t list_len(const list_t *h)
 {
-	size_t elements = 0; 
-			    
-	while (h) 
+	size_t elements = 0;
+
+	while (h)
 	{
-		elements++; 
-		h = h->next; 
+		elements++;
+		h = h->next;
 	}
 
-	return (elements); 
+	return (elements);
 }
--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,12 +1,13 @@
-#include <stdio.h> 
-  
- void housekeeping(void) __attribute__((constructor)); 
-   
- /** 
-  * housekeeping - does some action before the main function runs 
-  */ 
-  void housekeeping(void)
+#include <stdio.h>
+
+void housekeeping(void) __attribute__((constructor));
+
+/**
+ * housekeeping - does some action before the main function runs
+ **/
+
+void housekeeping(void)
 {
-	printf("You're beat! and yet, you must allow,\n"); 
-	printf("I bore my house upon my back!\n"); 
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
