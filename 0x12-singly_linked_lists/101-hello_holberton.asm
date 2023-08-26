1df0d43704d2f769cc234af484799a2dca52fa8c
SOLL <ibedemudo23@gmail.com>
2023-08-25 22:46:53 -0700

    0x12 C

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,28 +1,28 @@
-.file	"101-hello_holberton.c"
-	.text
-	.section	.rodata
-.LC0:
-	.string	"Hello, Holberton"
-	.text
-	.globl	main
-	.type	main, @function
-main:
-.LFB0:
-	.cfi_startproc
-	pushq	%rbp
-	.cfi_def_cfa_offset 16
-	.cfi_offset 6, -16
-	movq	%rsp, %rbp
-	.cfi_def_cfa_register 6
-	leaq	.LC0(%rip), %rax
-	movq	%rax, %rdi
-	call	puts@PLT
-	movl	$0, %eax
-	popq	%rbp
-	.cfi_def_cfa 7, 8
-	ret
-	.cfi_endproc
-.LFE0:
-	.size	main, .-main
-	.ident	"GCC: (Debian 11.3.0-5) 11.3.0"
-	.section
+extern printf; the C function, to be called
+
+
+      section.data; Data section, initialized variables
+msg:    db "Hello, Holberton", 0; C string needs 0
+fmt:    db "%s", 10, 0; The printf format, "\n", '0';
+
+
+       section.text; Code section.
+
+
+   global main; the standard gcc entry point
+   main:; the program label for the entry point
+   push    rbp     ; set up stack frame, must be alligned
+
+
+   mov rdi,fmt
+   mov rsi,msg
+   mov rax,0; or can be xor rax, rax
+   call printf; Call C function
+
+
+   pop rbp; restore stack
+
+
+   mov rax,0; normal, no error, return value
+   ret; return
+
